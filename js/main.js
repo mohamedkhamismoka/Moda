0761f64b29ee38e51a3ad6eb5ad5968ba744b2fa
mohameddatif <99369751+mohameddatif@users.noreply.github.com>
2022-10-17 02:35:16 +0200

    upday

--- js/main.js
+++ js/main.js
@@ -8,6 +8,20 @@ $("document").ready(function() {
 
     })
 
+    $(".to-top").click(function() {
+        window.scrollTo(0, 0);
+
+    })
+
+    $(".to-top").hover(function() {
+        $(this).css("opacity", "1")
+
+    })
+
+    $(".to-top").mouseleave(function() {
+        $(this).css("opacity", "0.5")
+
+    })
     $(window).scroll(function() {
         if (window.pageYOffset > 0) {
             $("nav").addClass("scroll");
@@ -15,6 +29,13 @@ $("document").ready(function() {
             $("nav").removeClass("scroll");
         }
 
+
+
+        if (window.scrollY > 480) {
+            $(".to-top").css("opacity", "0.7")
+        } else {
+            $(".to-top").css("opacity", "0")
+        }
     })
 
 })
