4145ca89d24e53d72e8553f698c47d51bb009dda
mohameddatif <99369751+mohameddatif@users.noreply.github.com>
2022-10-26 03:47:27 +0200

    bb

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ $("document").ready(function() {
     let spans = document.querySelectorAll(".progress span");
 
     window.onscroll = function() {
-        if (window.scrollY + 150 >= section.offsetTop) {
+        if (window.scrollY + 200 >= section.offsetTop) {
             console.log("Reached Section Three");
             spans.forEach((span) => {
                 span.style.width = span.dataset.width;
